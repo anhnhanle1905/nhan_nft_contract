29c6dd92c512412101cebca5ddb45ab0c926fb9a
anhnhanle1905 <75842089+anhnhanle1905@users.noreply.github.com>
2022-04-27 22:49:28 +0700

    test

--- test/testExample.js
+++ test/testExample.js
@@ -1,21 +1,21 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+// const { expect } = require("chai");
+// const { ethers } = require("hardhat");
 
-describe("Hello World", function () {
-  const message = "Hello world !!!!";
-  const messageSecond = "Bye world !!!!";
-  let [accountA, accountB, accountC] = [];
-  let helloWorld;
-  let address0 = "0x0000000000000000000000000000000000000000";
-  let uri = "sampleuri.com/";
-  beforeEach(async () => {
-    [accountA, accountB, accountC] = await ethers.getSigners();
-    const HelloWorld = await ethers.getContractFactory("HelloWorld");
-    helloWorld = await HelloWorld.deploy(message);
-    await helloWorld.deployed();
-  });
+// describe("Hello World", function () {
+//   const message = "Hello world !!!!";
+//   const messageSecond = "Bye world !!!!";
+//   let [accountA, accountB, accountC] = [];
+//   let helloWorld;
+//   let address0 = "0x0000000000000000000000000000000000000000";
+//   let uri = "sampleuri.com/";
+//   beforeEach(async () => {
+//     [accountA, accountB, accountC] = await ethers.getSigners();
+//     const HelloWorld = await ethers.getContractFactory("HelloWorld");
+//     helloWorld = await HelloWorld.deploy(message);
+//     await helloWorld.deployed();
+//   });
 
-  it("Should return message correctly", async function () {
-    expect(await helloWorld.printHelloWorld()).to.be.equal(message);
-  });
-});
+//   it("Should return message correctly", async function () {
+//     expect(await helloWorld.printHelloWorld()).to.be.equal(message);
+//   });
+// });
